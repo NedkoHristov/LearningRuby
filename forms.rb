0c27ecd4fed40e1cc320021b1bc7c368788132f1
NedkoHristov <nedelcho.hristov2001@gmail.com>
2020-03-27 13:23:53 +0200

    Forms, babe. Or form-ish, but still.
    I can us methods in `gets`. That's super awesome.

---
+++ forms.rb
@@ -0,0 +1,17 @@
+print "What's your first name?"
+first_name = gets.chomp
+puts first_name
+
+print "What's your last name?"
+last_name = gets.chomp
+# puts last_name
+
+print "What's your city, бе селянин?"
+city = gets.chomp
+# puts city
+
+print "What's your state, бе селянин?"
+state = gets.chomp.upcase
+# puts state
+
+puts first_name, last_name, city.upcase, state
