8060f8249d9cd48be168288e1794831b45351f9d
NedkoHristov <nedelcho.hristov2001@gmail.com>
2020-03-27 12:15:49 +0200

    Shittin' with upcase and downcase methods

--- methods.rb
+++ methods.rb
@@ -4,3 +4,19 @@ boobies = "I love boobies, but not long boobies"
 
 puts boobies
 puts boobies.length
+
+
+# Use method .reverse
+
+goofies = "Backward awkwardlessness"
+
+puts goofies
+puts goofies.reverse
+
+# Use upcase and downcase methods
+
+string_to_shit_about = "SoMe StRiNg gOeS HeRe"
+
+puts string_to_shit_about
+puts string_to_shit_about.upcase
+puts string_to_shit_about.downcase
