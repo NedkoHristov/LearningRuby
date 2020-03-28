5283f578be13e3969b6841a76445d8b9157e20d7
NedkoHristov <nedelcho.hristov2001@gmail.com>
2020-03-28 09:54:09 +0200

    Well I though that this section will be bigger, but ¯\_(ツ)_/¯

--- playing-with-strings.rb
+++ playing-with-strings.rb
@@ -4,6 +4,8 @@ user_input = gets.chomp
 if user_input.include? "s"
   user_input.gsub!(/s/, "th")
   puts user_input
+elsif user_input.include? "kur"
+  puts "Ей, педал, не ми се прави на интересен!"
 else
-  puts "your string DO NOT contain S"
+  puts "your string #{user_input} DOES NOT contain S"
 end
