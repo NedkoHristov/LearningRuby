906bea456a13aac47fa36f6b8858ad1b014ae9d0
NedkoHristov <nedelcho.hristov2001@gmail.com>
2020-04-03 13:40:16 +0300

    Single and multidementional arrays, bichachos

--- data-structures.rb
+++ data-structures.rb
@@ -17,3 +17,31 @@ puts my_hash["age"]
 puts my_hash["hungry?"]
 
 
+# print single and multidementional arrays
+friends = ["Milhouse", "Ralph", "Nelson", "Otto"]
+
+family = { "Homer" => "dad",
+           "Marge" => "mom",
+           "Lisa" => "sister",
+           "Maggie" => "sister",
+           "Abe" => "grandpa",
+           "Santa's Little Helper" => "dog"
+}
+
+friends.each { |x| puts "#{x}" }
+family.each { |x, y| puts "#{x}: #{y}" }
+
+
+# my example, yep, I don't have imagination
+array_1 = ["arr_value1", "arr_value2", "arr_value3"]
+
+array_2 = {
+    "key1" => "value1",
+    "key2" => "value2",
+    "key3" => "value3"
+}
+
+array_1.each {|a| puts "#{a}"}
+# puts "\n"
+array_2.each {|b, c| puts "#{b}", "#{c}"}
+# family.each  { |x, y| puts "#{x}: #{y}" }
